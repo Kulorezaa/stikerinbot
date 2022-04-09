7e36f0a4cadc727991af7cbcbde5611d5949367c
Kulorezaa <103280637+Kulorezaa@users.noreply.github.com>
2022-04-09 08:24:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let chalk = require('chalk')
 
-global.owner = ['6283128734012', '6287755080455'] // Letakan nomor kamu disini
+global.owner = ['62895412779117', '62895412779117'] // Letakan nomor kamu disini
 
 global.APIs = { // API Prefix
   // nama: 'https://website'
@@ -30,10 +30,10 @@ global.APIKeys = { // APIKey nya disini
 }
 
 // Sticker WM
-global.packname = 'arif️fb'
-global.author = 'amel'
+global.packname = 'ngont*l'
+global.author = 'amat'
 
-global.wm = '© ariffb & melcanz'
+global.wm = '© BukanWahyu'
 global.wait = '_*tunggu sedang diproses...*_'
 global.eror = '_*Server Error*_'
 global.benar = '✅'
